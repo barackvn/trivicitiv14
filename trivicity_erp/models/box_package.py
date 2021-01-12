a1445cb8bb1356a5cbf5481bb45b1cc8b5f24438
krishna.bhalala <krishna.bhalala@silentinfotech.com>
2021-01-12 18:14:44 +0530

    check availability for box packaging and assign NFC tag at mo done

--- trivicity_erp/models/box_package.py
+++ trivicity_erp/models/box_package.py
@@ -4,8 +4,11 @@ import datetime
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError, ValidationError, AccessError
 from xlrd import open_workbook
+from itertools import groupby
+from operator import itemgetter
 import base64
 import openpyxl
+from odoo.tools.float_utils import float_compare, float_is_zero, float_repr, float_round
 from tempfile import TemporaryFile
 
 
@@ -270,6 +273,7 @@ class BoxPackage(models.Model):
 
     def button_done(self):
         if not self.state == 'draft':
+            self.with_context(assign_lot=1).action_assign()
             mo_objs = self.env['mrp.production'].search([('box_package_id', '=', self.name),('state','in',('confirmed','progress','to_close'))])            
             if mo_objs:
                 if any(production.reservation_state != 'assigned' for production in mo_objs):
@@ -277,13 +281,11 @@ class BoxPackage(models.Model):
                 if all(production.reservation_state == 'assigned' for production in mo_objs):
                     for mo in mo_objs:
                         wiz_act = mo.button_mark_done()
-                        # print("wiz_act",wiz_act)
-                         # if wiz_act:
-                    #     wiz = self.env[wiz_act['res_model']].browse(wiz_act['context']['params']['id'])
-                    #     ans = wiz.process()
-                    #     print("ans",wiz_act, wiz, ans)
 
                 if all(production.state == 'done' for production in mo_objs):
+                    if mo_objs:
+                        for line in self.move_raw_ids:
+                            line.quantity_done = sum([move.quantity_done for move in mo_objs.mapped('move_raw_ids').filtered(lambda m: m.product_id.id == line.product_id.id)])
                     self.write({'state': 'done'})
                    
     def action_cancel(self):
@@ -340,36 +342,208 @@ class BoxPackage(models.Model):
         data = self.import_data_form_file()
         if data:
             index = 0                        
-            list_keys = list(data.keys())            
+            list_keys = list(data.keys())
             unique_list = []
-            for key_data in list_keys:                
+            mo_have_lot = mo_objs.mapped('lot_producing_id').mapped('name')
+            for key_data in list_keys:
                 dup_found = self.env['stock.production.lot'].search([('name','=', key_data)])
-                if not dup_found:
+                if not dup_found or key_data in mo_have_lot:
                     unique_list.append(key_data)
             for production in mo_objs:
-                if not production.lot_producing_id:
-                    if len(unique_list) > index:                       
-                        lot = self.env['stock.production.lot'].create({
-                            'name': unique_list[index],
-                            'product_id': self.product_id.id,
-                            'company_id': self.env.company.id,
-                        })                        
-                        index += 1
-                        production.write({
-                            'lot_producing_id': lot.id,
-                        })
-                    else:
-                        raise UserError(_('File lot is not enough Please upload another lots.'))
-                if production.lot_producing_id:
-                    production.move_raw_ids._action_assign()
-                    for move in production.move_raw_ids:                        
-                        for move_line in move.move_line_ids:                            
-                            move_line.write({
-                                'qty_done': move_line.product_uom_qty
+                if len(unique_list) > index:
+                    lot_name = data.get(unique_list[index])
+                    self._action_assign(production, lot_name)
+                    index += 1
+                else:
+                    raise UserError(_('File lot is not enough Please upload another lots.'))
+            if self._context.get('assign_lot'):
+                index = 0
+                for production in mo_objs:
+                    if not production.lot_producing_id:
+                        if len(unique_list) > index:
+                            lot = self.env['stock.production.lot'].create({
+                                'name': unique_list[index],
+                                'product_id': self.product_id.id,
+                                'company_id': self.env.company.id,
                             })
-                    if production.product_id.tracking == 'serial':
-                        result = production._set_qty_producing()                        
+                            index += 1
+                            production.write({
+                                'lot_producing_id': lot.id,
+                            })
+                            production._onchange_producing()
+                            production._onchange_lot_producing()
+                            for line in production.move_raw_ids.mapped('move_line_ids'):
+                                if line.product_uom_qty != line.qty_done:
+                                    line.qty_done = line.product_uom_qty
+
+        else:
+            raise ValidationError(_("Please upload 2 Columns NFC tag and Lot information with data."))
         return True
+
+    def _action_assign(self, mo_obj, lot_name):
+        move_ids = mo_obj.move_raw_ids
+        assigned_moves = self.env['stock.move']
+        partially_available_moves = self.env['stock.move']
+        # Read the `reserved_availability` field of the moves out of the loop to prevent unwanted
+        # cache invalidation when actually reserving the move.
+        reserved_availability = {move: move.reserved_availability for move in move_ids}
+        roundings = {move: move.product_id.uom_id.rounding for move in move_ids}
+        move_line_vals_list = []
+        not_available_list = []
+        for move in move_ids.filtered(lambda m: m.state != 'cancel'):
+            rounding = roundings[move]
+            missing_reserved_uom_quantity = move.product_uom_qty - reserved_availability[move]
+            missing_reserved_quantity = move.product_uom._compute_quantity(missing_reserved_uom_quantity,
+                                                                           move.product_id.uom_id,
+                                                                           rounding_method='HALF-UP')
+            if move._should_bypass_reservation():
+                # create the move line(s) but do not impact quants
+                if move.product_id.tracking == 'serial' and (
+                        move.picking_type_id.use_create_lots or move.picking_type_id.use_existing_lots):
+                    for i in range(0, int(missing_reserved_quantity)):
+                        move_line_vals_list.append(move._prepare_move_line_vals(quantity=1))
+                else:
+                    to_update = move.move_line_ids.filtered(lambda ml: ml.product_uom_id == move.product_uom and
+                                                                       ml.location_id == move.location_id and
+                                                                       ml.location_dest_id == move.location_dest_id and
+                                                                       ml.picking_id == move.picking_id and
+                                                                       not ml.lot_id and
+                                                                       not ml.package_id and
+                                                                       not ml.owner_id)
+                    if to_update:
+                        to_update[0].product_uom_qty += missing_reserved_uom_quantity
+                    else:
+                        move_line_vals_list.append(move._prepare_move_line_vals(quantity=missing_reserved_quantity))
+                assigned_moves |= move
+            else:
+                if float_is_zero(move.product_uom_qty, precision_rounding=move.product_uom.rounding):
+                    assigned_moves |= move
+                elif not move.move_orig_ids:
+                    if move.procure_method == 'make_to_order':
+                        continue
+                    # If we don't need any quantity, consider the move assigned.
+                    need = missing_reserved_quantity
+                    lot_id = None
+                    if move.product_id.tracking == 'none':
+                        if float_is_zero(need, precision_rounding=rounding):
+                            assigned_moves |= move
+                            continue
+                    else:
+                        move_line = move.move_line_ids
+                        if move_line and move_line.filtered(lambda m: m.lot_id.name == lot_name or m.lot_name == lot_name):
+                            if float_is_zero(need, precision_rounding=rounding):
+                                assigned_moves |= move
+                                continue
+                            lot_id = move_line[0].lot_id
+                        else:
+                            move_line.unlink()
+                            need = move.product_uom_qty
+                    # Reserve new quants and create move lines accordingly.
+                    if not lot_id and move.product_id.tracking != 'none':
+                        lot_id = self.env['stock.production.lot'].search([('product_id', '=', move.product_id.id), ('name', '=', lot_name), ('product_qty', '>=', need)])
+                        if not lot_id:
+                            not_available_list.append(lot_name)
+                            continue
+                    forced_package_id = move.package_level_id.package_id or None
+                    available_quantity = move._get_available_quantity(move.location_id, lot_id=lot_id, package_id=forced_package_id)
+                    if available_quantity < need:
+                        not_available_list.append(lot_name)
+                        continue
+                    taken_quantity = move._update_reserved_quantity(need, available_quantity, move.location_id, lot_id=lot_id,
+                                                                    package_id=forced_package_id, strict=False)
+                    if float_is_zero(taken_quantity, precision_rounding=rounding):
+                        continue
+                    if float_compare(need, taken_quantity, precision_rounding=rounding) == 0:
+                        assigned_moves |= move
+                    else:
+                        partially_available_moves |= move
+                else:
+                    # Check what our parents brought and what our siblings took in order to
+                    # determine what we can distribute.
+                    # `qty_done` is in `ml.product_uom_id` and, as we will later increase
+                    # the reserved quantity on the quants, convert it here in
+                    # `product_id.uom_id` (the UOM of the quants is the UOM of the product).
+                    move_lines_in = move.move_orig_ids.filtered(lambda m: m.state == 'done').mapped('move_line_ids')
+                    keys_in_groupby = ['location_dest_id', 'lot_id', 'result_package_id', 'owner_id']
+
+                    def _keys_in_sorted(ml):
+                        return (ml.location_dest_id.id, ml.lot_id.id, ml.result_package_id.id, ml.owner_id.id)
+
+                    grouped_move_lines_in = {}
+                    for k, g in groupby(sorted(move_lines_in, key=_keys_in_sorted), key=itemgetter(*keys_in_groupby)):
+                        qty_done = 0
+                        for ml in g:
+                            qty_done += ml.product_uom_id._compute_quantity(ml.qty_done, ml.product_id.uom_id)
+                        grouped_move_lines_in[k] = qty_done
+                    move_lines_out_done = (move.move_orig_ids.mapped('move_dest_ids') - move) \
+                        .filtered(lambda m: m.state in ['done']) \
+                        .mapped('move_line_ids')
+                    # As we defer the write on the stock.move's state at the end of the loop, there
+                    # could be moves to consider in what our siblings already took.
+                    moves_out_siblings = move.move_orig_ids.mapped('move_dest_ids') - move
+                    moves_out_siblings_to_consider = moves_out_siblings & (assigned_moves + partially_available_moves)
+                    reserved_moves_out_siblings = moves_out_siblings.filtered(
+                        lambda m: m.state in ['partially_available', 'assigned'])
+                    move_lines_out_reserved = (reserved_moves_out_siblings | moves_out_siblings_to_consider).mapped(
+                        'move_line_ids')
+                    keys_out_groupby = ['location_id', 'lot_id', 'package_id', 'owner_id']
+
+                    def _keys_out_sorted(ml):
+                        return (ml.location_id.id, ml.lot_id.id, ml.package_id.id, ml.owner_id.id)
+
+                    grouped_move_lines_out = {}
+                    for k, g in groupby(sorted(move_lines_out_done, key=_keys_out_sorted),
+                                        key=itemgetter(*keys_out_groupby)):
+                        qty_done = 0
+                        for ml in g:
+                            qty_done += ml.product_uom_id._compute_quantity(ml.qty_done, ml.product_id.uom_id)
+                        grouped_move_lines_out[k] = qty_done
+                    for k, g in groupby(sorted(move_lines_out_reserved, key=_keys_out_sorted),
+                                        key=itemgetter(*keys_out_groupby)):
+                        grouped_move_lines_out[k] = sum(
+                            self.env['stock.move.line'].concat(*list(g)).mapped('product_qty'))
+                    available_move_lines = {key: grouped_move_lines_in[key] - grouped_move_lines_out.get(key, 0) for key
+                                            in grouped_move_lines_in.keys()}
+                    # pop key if the quantity available amount to 0
+                    available_move_lines = dict((k, v) for k, v in available_move_lines.items() if v)
+
+                    if not available_move_lines:
+                        continue
+                    for move_line in move.move_line_ids.filtered(lambda m: m.product_qty):
+                        if available_move_lines.get((move_line.location_id, move_line.lot_id,
+                                                     move_line.result_package_id, move_line.owner_id)):
+                            available_move_lines[(move_line.location_id, move_line.lot_id, move_line.result_package_id,
+                                                  move_line.owner_id)] -= move_line.product_qty
+                    for (location_id, lot_id, package_id, owner_id), quantity in available_move_lines.items():
+                        need = move.product_qty - sum(move.move_line_ids.mapped('product_qty'))
+                        # `quantity` is what is brought by chained done move lines. We double check
+                        # here this quantity is available on the quants themselves. If not, this
+                        # could be the result of an inventory adjustment that removed totally of
+                        # partially `quantity`. When this happens, we chose to reserve the maximum
+                        # still available. This situation could not happen on MTS move, because in
+                        # this case `quantity` is directly the quantity on the quants themselves.
+                        available_quantity = move._get_available_quantity(location_id, lot_id=lot_id,
+                                                                          package_id=package_id, owner_id=owner_id,
+                                                                          strict=True)
+                        if float_is_zero(available_quantity, precision_rounding=rounding):
+                            continue
+                        taken_quantity = move._update_reserved_quantity(need, min(quantity, available_quantity),
+                                                                        location_id, lot_id, package_id, owner_id)
+                        if float_is_zero(taken_quantity, precision_rounding=rounding):
+                            continue
+                        if float_is_zero(need - taken_quantity, precision_rounding=rounding):
+                            assigned_moves |= move
+                            break
+                        partially_available_moves |= move
+            if move.product_id.tracking == 'serial':
+                move.next_serial_count = move.product_uom_qty
+        if not_available_list:
+            raise UserError(_('Stock not available %s', (", ".join(a for a in set(not_available_list)))))
+
+        self.env['stock.move.line'].create(move_line_vals_list)
+        partially_available_moves.write({'state': 'partially_available'})
+        assigned_moves.write({'state': 'assigned'})
+        move_ids.mapped('picking_id')._check_entire_pack()
 
     def import_data_form_file(self):        
         # Generating of the excel file to be read by openpyxl
@@ -390,7 +564,6 @@ class BoxPackage(models.Model):
             data[row[0].value] = row[1].value        
         return data
 
-
     def _check_filename(self):
         if self.upload_file:
             if not self.filename:
@@ -403,7 +576,6 @@ class BoxPackage(models.Model):
                     raise ValidationError(_("The file must be a xlsx file"))
         else:
             raise ValidationError(_("Please upload NFC tag and Lot information."))
-
 
     def unlink(self):
         for record in self:
